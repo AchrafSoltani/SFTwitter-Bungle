3fdc1a4b6ee633234c98bd34ccd6ecb92cee5034
Achraf <achraf.soltani@gmail.com>
2013-11-15 21:27:47 +0000

    base wrapper class

---
+++ src/SFTwitter/SFTwitter.php
@@ -0,0 +1,20 @@
+<?php
+ /**
+ * Twitter
+ *
+ * A wrapper class for the Twitter API integrated to Symfony 2
+ *
+ * @package		SFTwitter-Bungle
+ * @subpackage	Vendor
+ * @category	Library
+ * @author		Achraf Soltani <soltani.achraf@gmail.com>
+ * @link		http://www.achrafsoltani.com/
+ * @date        11/15/2013
+ */
+
+namespace SFTwitter;
+
+class SFTwitter
+{
+
+} 
